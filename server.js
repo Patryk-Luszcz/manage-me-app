dc111bc4f5e68067c2cdb72dd92d7d54e837fc23
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-12 01:38:32 +0200

    feat: implement registration logic in server js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const jwt = require('jsonwebtoken');
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 
-//LOGOWANIE
+//LOIGN AND REGISTER
 
-server.post('/users', (req, res) => {
+server.post('/users/login', (req, res) => {
   const { login, password } = req.body;
   const users = router.db.get('users').value();
 
@@ -25,4 +25,44 @@ server.post('/users', (req, res) => {
   } else {
     res.status(401).jsonp({ error: 'Invalid credentials' });
   }
+});
+
+server.post('/users/register', (req, res) => {
+  const newUser = req.body;
+  newUser.role = { name: 'developer' };
+
+  const users = router.db.get('users');
+  const existingUser = users.find({ login: newUser.login }).value();
+
+  if (existingUser) {
+    res.status(409).json({ error: 'User already exists' });
+    return;
+  }
+
+  const allUsers = users.value();
+  const lastUserId = allUsers.length > 0 ? allUsers[allUsers.length - 1].id : 0;
+
+  const generatedId = lastUserId + 1;
+  const authorizationToken = jwt.sign({ userId: generatedId }, 'my-secret-key');
+
+  const userWithId = {
+    id: generatedId,
+    firstName: newUser.firstName,
+    lastName: newUser.lastName,
+    login: newUser.login,
+    authorizationToken: authorizationToken,
+    role: {
+      name: 'developer',
+    },
+  };
+  users.push(userWithId).write();
+
+  res.status(201).json(userWithId);
+});
+
+// PORT LISTENER
+
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
 });
