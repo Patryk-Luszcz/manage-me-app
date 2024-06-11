75faed43c4e7b9d90c8c4aaf5a48c97ae4af3847
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-12 01:58:06 +0200

    feat: add new path projects in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { authenticationGuard } from '../shared/guards/authentication.guard';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: AuthenticationComponent },
 
+  { path: 'login', component: AuthenticationComponent },
   { path: 'registration', component: AuthenticationComponent },
+  { path: 'projects', component: ProjectsComponent, canActivate: [authenticationGuard] },
 ];
