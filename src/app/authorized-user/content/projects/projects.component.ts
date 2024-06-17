e8adee2dc96e68ebc0d62612b0c208f28ab2ddc2
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-18 00:08:23 +0200

    feat: implement projects table list

--- src/app/authorized-user/content/projects/projects.component.ts
+++ src/app/authorized-user/content/projects/projects.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ProjectService } from '../../../../shared/services/project.service';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { Observable } from 'rxjs';
+import { TableComponent } from '../../../../shared/components/table/table.component';
+import { Project } from '../../../../shared/interfaces/project.interface';
+import { TableConfig } from '../../../../shared/components/table/table.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgFor, TableComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects$!: Observable<Project[]>;
 
+  tableConfig: TableConfig<Project>[] = [
+    {
+      title: 'Name',
+      modelProp: 'name',
+    },
+    {
+      title: 'Description',
+      modelProp: 'description',
+    },
+  ];
+
+  private _projectService = inject(ProjectService);
+
+  public ngOnInit() {
+    this.projects$ = this._projectService.getProjects();
+  }
 }
