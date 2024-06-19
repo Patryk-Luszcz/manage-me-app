6e3ba1e6b096bcffbaabdfe1b6bd6d0f16e6abec
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-20 00:11:40 +0200

    feat: create route path for functionalities section

--- src/app/authorized-user/content/content.routes.ts
+++ src/app/authorized-user/content/content.routes.ts
@@ -3,10 +3,12 @@ import { ProjectsComponent } from './projects/projects.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { AdminComponent } from './admin/admin.component';
 import { adminGuard } from '../../../shared/guards/admin.guard';
+import { FunctionalitiesComponent } from './functionalities/functionalities.component';
 
 export const contentRoutes: Routes = [
   { path: '', redirectTo: 'projects', pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'functionalities', component: FunctionalitiesComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
 ];
