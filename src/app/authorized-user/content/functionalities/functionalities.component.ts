492b69669858aad3795610d6d477dbe652192e25
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-20 00:12:07 +0200

    feat: implement logic to display functionalities list

--- src/app/authorized-user/content/functionalities/functionalities.component.ts
+++ src/app/authorized-user/content/functionalities/functionalities.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TableComponent, TableConfig } from '../../../../shared/components/table/table.component';
+import { FunctionalityService } from '../../../../shared/services/functionality.service';
+import { Functionality } from '../../../../shared/interfaces/functionality.inteface';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-functionalities',
   standalone: true,
-  imports: [],
+  imports: [TableComponent, AsyncPipe],
   templateUrl: './functionalities.component.html',
-  styleUrl: './functionalities.component.scss'
+  styleUrl: './functionalities.component.scss',
 })
 export class FunctionalitiesComponent {
+  private _functionalityService = inject(FunctionalityService);
 
+  functionalities$ = this._functionalityService.getFunctionalities();
+
+  functionalityTableConfig: TableConfig<Functionality>[] = [
+    {
+      title: 'Name',
+      modelProp: 'name',
+    },
+    {
+      title: 'Description',
+      modelProp: 'description',
+    },
+    {
+      title: 'Project',
+      modelProp: 'project',
+    },
+    {
+      title: 'Status',
+      modelProp: 'status',
+    },
+    {
+      title: 'Owner',
+      modelProp: 'owner',
+    },
+    {
+      title: 'Priority',
+      modelProp: 'priority',
+    },
+  ];
 }
