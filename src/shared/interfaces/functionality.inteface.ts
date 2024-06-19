11e45d80b6056daff8856b2f8d2862dbebded7c9
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-19 23:52:05 +0200

    feat: create functionality service

---
+++ src/shared/interfaces/functionality.inteface.ts
@@ -0,0 +1,11 @@
+export interface Functionality {
+  id: number;
+  projectId: number;
+  name: string;
+  description: string;
+  priority: string;
+  project: string;
+  owner: string;
+  status: string;
+  tasks: any[];
+}
