e793f2788263e1e48fc03d62e0c4aa14e02d1505
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-21 00:13:16 +0200

    feat: create global constants file and add there actionsConfig

---
+++ src/shared/constants/global.constants.ts
@@ -0,0 +1,19 @@
+export abstract class GlobalConstants {
+  static readonly actionsConfig = [
+    {
+      title: 'INFO',
+      iconSrc: '/icons/info.svg',
+      tooltip: 'More Info',
+    },
+    {
+      title: 'EDIT',
+      iconSrc: '/icons/edit.svg',
+      tooltip: 'Edit',
+    },
+    {
+      title: 'DELETE',
+      iconSrc: '/icons/delete.svg',
+      tooltip: 'Delete',
+    },
+  ];
+}
