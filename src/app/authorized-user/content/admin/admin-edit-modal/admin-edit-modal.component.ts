5579d68bd58764eccd898ac53fe5159c2d2a4872
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-25 22:04:47 +0200

    feat: create admin edit modal component

---
+++ src/app/authorized-user/content/admin/admin-edit-modal/admin-edit-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-edit-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-edit-modal.component.html',
+  styleUrl: './admin-edit-modal.component.scss'
+})
+export class AdminEditModalComponent {
+
+}
