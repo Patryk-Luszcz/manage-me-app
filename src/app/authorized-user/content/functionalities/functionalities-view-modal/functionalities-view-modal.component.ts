74cd79ad231dab86b10280e12b4bda6e43750daa
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-25 22:07:14 +0200

    feat: create functionalities view modal

---
+++ src/app/authorized-user/content/functionalities/functionalities-view-modal/functionalities-view-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-functionalities-view-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './functionalities-view-modal.component.html',
+  styleUrl: './functionalities-view-modal.component.scss'
+})
+export class FunctionalitiesViewModalComponent {
+
+}
