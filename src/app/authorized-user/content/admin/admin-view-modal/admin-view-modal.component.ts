31e47b1df4693be717aa408987f9aa080c73968e
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-06-25 22:05:06 +0200

    feat: create admin view modal component

---
+++ src/app/authorized-user/content/admin/admin-view-modal/admin-view-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-view-modal.component.html',
+  styleUrl: './admin-view-modal.component.scss'
+})
+export class AdminViewModalComponent {
+
+}
