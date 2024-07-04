1b9e50e00cdd2c8771abb7161d9f862a62e8abbf
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-07-04 22:40:48 +0200

    feat: implement admin view modal

--- src/app/authorized-user/content/admin/admin-view-modal/admin-view-modal.component.ts
+++ src/app/authorized-user/content/admin/admin-view-modal/admin-view-modal.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalComponent } from '../../../../../shared/components/modals/modal/modal.component';
 
 @Component({
   selector: 'app-admin-view-modal',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './admin-view-modal.component.html',
-  styleUrl: './admin-view-modal.component.scss'
+  styleUrl: './admin-view-modal.component.scss',
 })
 export class AdminViewModalComponent {
-
+  modalData = inject(MAT_DIALOG_DATA);
 }
